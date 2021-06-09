06bb61abaad6b24b333d32d2350cd5651952d571
LinoR <robertlino783@gmail.com>
2021-06-10 02:23:32 +0500

    make it better

---
+++ src/Api/api.js
@@ -0,0 +1,89 @@
+import axios from 'axios';
+
+export const CallumAPI = {
+  celebrities: [
+
+  ],
+  celebrity: {},
+  all: function () {
+    return axios.get('/profiles', {
+
+
+    })
+      .then(res => {
+        this.celebrities = res.data;
+        return this.celebrities;
+      })
+  },
+
+  getCelebrity: function (id) {
+    return axios.get(`/profiles/${id}`, {
+
+
+    })
+      .then(res => {
+        this.celebrity = res.data;
+        return this.celebrity;
+      })
+  },
+
+  addCelebrity: function (celebrity) {
+    const formData = new FormData();
+    formData.append('firstName', celebrity.firstName);
+    formData.append('lastName', celebrity.lastName);
+    formData.append('address', celebrity.address);
+    formData.append('gender', celebrity.gender);
+    formData.append('file', celebrity?.file);
+    formData.append('age', celebrity?.age)
+    formData.append('facebook',celebrity?.facebook);
+    formData.append('instagram',celebrity?.instahgram);
+    formData.append('twitter',celebrity?.twitter);
+    formData.append('email',celebrity?.email);
+    formData.append('password',celebrity?.password);
+    return axios.post('/profiles', formData, {
+
+    }).then(
+      res => {
+        this.celebrity = res.data;
+        return this.celebrity;
+      }
+    )
+
+
+  },
+
+  editCelebrity: function (celebrity, id) {
+    const formData = new FormData();
+    formData.append('firstName', celebrity.firstName);
+    formData.append('lastName', celebrity.lastName);
+    formData.append('address', celebrity.address);
+    formData.append('gender', celebrity.gender);
+    formData.append('age', celebrity?.age)
+    formData.append('facebook',celebrity?.facebook);
+    formData.append('instagram',celebrity?.instahgram);
+    formData.append('twitter',celebrity?.twitter);
+    formData.append('email',celebrity?.email);
+    formData.append('likes[]',celebrity?.likes);
+    formData.append('password',celebrity?.facebook);
+    formData.append('age', celebrity?.age);
+    formData.append('file',celebrity.file);
+    return axios.put(`/profiles/${id}`, formData, {
+
+    }).then(
+      res => {
+        this.celebrity = res.data;
+        return this.celebrity;
+      }
+    )
+
+
+  },
+  deleteCelebrity: function (id) {
+    return axios.delete(`/profiles/${id}`).then(
+      res => {
+        this.celebrity = res.data;
+        return this.celebrity;
+      }
+    )
+  }
+}
